2ae7db301695668f1ba6ea5f748b40d48501f451
marcel <marcelinocost69@gmail.com>
2023-02-17 08:42:25 -0300

    commit - 13

--- db.js
+++ db.js
@@ -20,29 +20,29 @@ async function findCustomers() {
   await global.connection.collection('customers').find({}).toArray()
 }
 
-async function findCustomer(id) {
+function findCustomer(id) {
   connectDatabase()
   const objectId = new ObjectId(id)
-  await global.connection.collection('customers').findOne({ _id: objectId })
+  global.connection.collection('customers').findOne({ _id: objectId })
 }
 
-async function insertCustomers(customers) {
+function insertCustomers(customers) {
   connectDatabase()
-  await global.connection.collection('customers').insertOne(customers)
+  global.connection.collection('customers').insertOne(customers)
 }
 
-async function updateCustomers(id, customers) {
+function updateCustomers(id, customers) {
   connectDatabase()
   const objectId = new ObjectId(id)
-  await global.connection
+  global.connection
     .collection('customers')
     .updateOne({ _id: objectId }, { $set: customers })
 }
 
-async function deleteCustomers(id) {
+function deleteCustomers(id) {
   connectDatabase()
   const objectId = new ObjectId(id)
-  await global.connection.collection('customers').deleteOne({ _id: objectId })
+  global.connection.collection('customers').deleteOne({ _id: objectId })
 }
 
 module.exports = {
